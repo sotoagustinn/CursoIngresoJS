dd0145cf29155645ba47d80ba313a90e01914fa3
Agustin Soto <sotoagustinn18@gmail.com>
2021-07-07 11:41:26 -0300

    Primer Prueba de Mi PC

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Curso de Ingreso UTN");
+	alert("Esto funciona de maravilla");
 }
 
